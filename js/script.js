95ba0a4d7aabc4814d5b97c76513f230cc47c0c5
paul172v <78083682+paul172v@users.noreply.github.com>
2021-07-19 22:37:09 +0100

    Update script.js
    Refactored my javascript code.

--- js/script.js
+++ js/script.js
@@ -5,27 +5,50 @@
 let currentGalleryImg = 1;
 
 ////////////////////////////
+/*--- Reusable Functions ---*/
+////////////////////////////
+
+const updateGalleyFullscreenImage = function () {
+  document.querySelector(
+    ".gal-img-large"
+  ).src = `img/fullscreen/gal${currentGalleryImg}.jpg`;
+};
+
+const updateGalleryFullscreenOrientation = function (imageNum) {
+  if (imageNum == 1 || imageNum == 5) {
+    document.querySelector(".gal-img-large").classList.remove("land");
+    document.querySelector(".gal-img-large").classList.add("port");
+  } else {
+    document.querySelector(".gal-img-large").classList.remove("port");
+    document.querySelector(".gal-img-large").classList.add("land");
+  }
+};
+
+const hideAllModals = function () {
+  document.querySelector(".modal-background").style.display = "none";
+  document.querySelector(".modal-gal-img").style.display = "none";
+  document.querySelector(".btn-close-gal").style.display = "none";
+  document.querySelector(".modal-text-box-popup").style.display = "none";
+};
+
+const showTextPopup = function (message) {
+  document.querySelector(".modal-popup-text ").textContent = message;
+  document.querySelector(".modal-background").style.display = "inline-block";
+  document.querySelector(".modal-text-box-popup").style.display = "flex";
+};
+
+////////////////////////////
 /*--- Modal Gallery ---*/
 ////////////////////////////
 
 //// Clicking a thumbnail
 document.querySelectorAll(".gal-img").forEach((el) => {
   el.addEventListener("click", function () {
+    console.log(el);
     palceholderValue = el.alt;
-
     currentGalleryImg = `${palceholderValue}`.replace("gallery image ", "");
-
-    document.querySelector(
-      ".gal-img-large"
-    ).src = `img/fullscreen/gal${currentGalleryImg}.jpg`;
-
-    if (currentGalleryImg.class === "gal1.jpg" || "gal5.jpg") {
-      document.querySelector(".gal-img-large").classList.add("port");
-      document.querySelector(".gal-img-large").classList.remove("land");
-    } else {
-      document.querySelector(".gal-img-large").classList.remove("port");
-      document.querySelector(".gal-img-large").classList.add("land");
-    }
+    updateGalleryFullscreenOrientation(currentGalleryImg);
+    updateGalleyFullscreenImage();
 
     document.querySelector(".modal-background").style.display = "inline-block";
     document.querySelector(".modal-gal-img").style.display = "inline-block";
@@ -37,40 +60,24 @@ document.querySelectorAll(".gal-img").forEach((el) => {
 document
   .querySelector(".modal-background")
   .addEventListener("click", function () {
-    document.querySelector(".modal-background").style.display = "none";
-    document.querySelector(".modal-gal-img").style.display = "none";
-    document.querySelector(".btn-close-gal").style.display = "none";
-    document.querySelector(".modal-text-box-popup").style.display = "none";
+    hideAllModals();
   });
 
 //// clicking the modal close button to close everything
 document.querySelector(".btn-close-gal").addEventListener("click", function () {
-  document.querySelector(".modal-background").style.display = "none";
-  document.querySelector(".modal-gal-img").style.display = "none";
-  document.querySelector(".btn-close-gal").style.display = "none";
+  hideAllModals();
 });
 
 //// clicking the modal gallery prev button
 document.querySelector(".btn-prev").addEventListener("click", function () {
   if (currentGalleryImg == 1) {
     currentGalleryImg = 10;
-
-    document.querySelector(
-      ".gal-img-large"
-    ).src = `img/fullscreen/gal${currentGalleryImg}.jpg`;
+    updateGalleryFullscreenOrientation(currentGalleryImg);
+    updateGalleyFullscreenImage();
   } else {
     currentGalleryImg--;
-    document.querySelector(
-      ".gal-img-large"
-    ).src = `img/fullscreen/gal${currentGalleryImg}.jpg`;
-  }
-
-  if (currentGalleryImg.class === "gal1.jpg" || "gal5.jpg") {
-    document.querySelector(".gal-img-large").classList.add("port");
-    document.querySelector(".gal-img-large").classList.remove("land");
-  } else {
-    document.querySelector(".gal-img-large").classList.remove("port");
-    document.querySelector(".gal-img-large").classList.add("land");
+    updateGalleryFullscreenOrientation(currentGalleryImg);
+    updateGalleyFullscreenImage();
   }
 });
 
@@ -78,40 +85,22 @@ document.querySelector(".btn-prev").addEventListener("click", function () {
 document.querySelector(".btn-next").addEventListener("click", function () {
   if (currentGalleryImg == 10) {
     currentGalleryImg = 1;
-
-    document.querySelector(
-      ".gal-img-large"
-    ).src = `img/fullscreen/gal${currentGalleryImg}.jpg`;
+    updateGalleryFullscreenOrientation(currentGalleryImg);
+    updateGalleyFullscreenImage();
   } else {
     currentGalleryImg++;
-    document.querySelector(
-      ".gal-img-large"
-    ).src = `img/fullscreen/gal${currentGalleryImg}.jpg`;
-  }
-
-  if (currentGalleryImg.class === "gal1.jpg" || "gal5.jpg") {
-    document.querySelector(".gal-img-large").classList.add("port");
-    document.querySelector(".gal-img-large").classList.remove("land");
-  } else {
-    document.querySelector(".gal-img-large").classList.remove("port");
-    document.querySelector(".gal-img-large").classList.add("land");
+    updateGalleryFullscreenOrientation(currentGalleryImg);
+    updateGalleyFullscreenImage();
   }
 });
 
 ////////////////////////
-////// nav-links ///////
+////// Nav-links ///////
 ////////////////////////
 
-const navLinks = function () {
+const activateNavLinks = function () {
   // Selectors
-
-  const sectionDormsBounding = document.getElementById("sectionDorms");
-  const sectionFacilitiesBounding =
-    document.getElementById("sectionFacilities");
-  const sectionGalleryBounding = document.getElementById("sectionGallery");
-  const sectionLocationBounding = document.getElementById("sectionLocation");
-  const sectionContactBounding = document.getElementById("sectionContact");
-
+  const scrollToTop = document.getElementById("scrollToTop");
   const scrollToDorms = document.getElementById("scrollToDorms");
   const scrollToFacilities = document.getElementById("scrollToFacilities");
   const scrollToGallery = document.getElementById("scrollToGallery");
@@ -120,7 +109,7 @@ const navLinks = function () {
 
   const navOffset = document.querySelector(".nav-bar").offsetHeight;
 
-  // Functions
+  // Function
   const scrollToSection = function (section) {
     let currentScollPosition = 0;
     let x = section.getBoundingClientRect();
@@ -136,6 +125,14 @@ const navLinks = function () {
   };
 
   // Scroll to Buttons
+  scrollToTop.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+
   scrollToDorms.addEventListener("click", function () {
     scrollToSection(sectionDorms);
   });
@@ -164,10 +161,9 @@ const navLinks = function () {
 // Book Now Button
 document.querySelectorAll(".btn-book-now").forEach((el) => {
   el.addEventListener("click", function () {
-    document.querySelector(".modal-popup-text ").textContent =
-      "The Book Now button would take you to Hostelworld to make a booking, this feature is not included as part of the London Wayfarer portfolio piece.";
-    document.querySelector(".modal-background").style.display = "inline-block";
-    document.querySelector(".modal-text-box-popup").style.display = "flex";
+    showTextPopup(
+      "The Book Now button would take you to Hostelworld to make a booking, this feature is not included as part of the London Wayfarer portfolio piece."
+    );
   });
 });
 
@@ -175,21 +171,19 @@ document.querySelectorAll(".btn-book-now").forEach((el) => {
 document
   .querySelector(".btn-close-popup")
   .addEventListener("click", function () {
-    document.querySelector(".modal-background").style.display = "none";
-    document.querySelector(".modal-text-box-popup").style.display = "none";
+    hideAllModals();
   });
 
-//////////////////////////////////
+////////////////////////////////////
 /*--- Modal Social Icons Popup ---*/
 ////////////////////////////////////
 
 // Book Now Button
 document.querySelectorAll(".icon").forEach((el) => {
   el.addEventListener("click", function () {
-    document.querySelector(".modal-popup-text ").textContent =
-      "This icon button would take you to the hostel's social media channels. This feature is not included as part of the London Wayfarer portfolio piece.";
-    document.querySelector(".modal-background").style.display = "inline-block";
-    document.querySelector(".modal-text-box-popup").style.display = "flex";
+    showTextPopup(
+      "This icon button would take you to the hostel's social media channels. This feature is not included as part of the London Wayfarer portfolio piece."
+    );
   });
 });
 
@@ -197,9 +191,10 @@ document.querySelectorAll(".icon").forEach((el) => {
 document
   .querySelector(".btn-close-popup")
   .addEventListener("click", function () {
-    document.querySelector(".modal-background").style.display = "none";
-    document.querySelector(".modal-text-box-popup").style.display = "none";
+    hideAllModals();
   });
 
-//// Trigger
-navLinks();
+////////////////////
+/*--- Triggers ---*/
+///////////////////
+activateNavLinks();
